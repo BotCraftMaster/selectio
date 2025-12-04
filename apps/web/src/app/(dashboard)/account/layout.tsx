eccddd162b2707db515dde8d978f04886a5baea9
Maksim <kodermax@gmail.com>
2025-12-04 20:09:06 +0300

    fix

--- apps/web/src/app/(dashboard)/account/layout.tsx
+++ apps/web/src/app/(dashboard)/account/layout.tsx
@@ -1,5 +1,11 @@
 import type { ReactNode } from "react";
+import { SiteHeader } from "~/components/layout";
 
 export default function AccountsLayout({ children }: { children: ReactNode }) {
-  return <div className="container max-w-4xl py-8">{children}</div>;
+  return (
+    <>
+      <SiteHeader title="" />
+      <div className="container max-w-4xl py-8">{children}</div>
+    </>
+  );
 }
